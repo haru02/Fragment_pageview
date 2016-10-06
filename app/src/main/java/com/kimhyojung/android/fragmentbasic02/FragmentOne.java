a9505d19b9b63a817633251e0f4e735b8f4a02ea
SEC\USER <hyojung0220.kim@gmail.com>
2016-10-06 17:56:50 +0900

    android fragment pageview

---
+++ app/src/main/java/com/kimhyojung/android/fragmentbasic02/FragmentOne.java
@@ -0,0 +1,42 @@
+package com.kimhyojung.android.fragmentbasic02;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentOne extends Fragment {
+
+
+    public FragmentOne() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_fragment_one, container, false);
+        Button button = (Button)view.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity mainActivity = (MainActivity) getActivity();
+                mainActivity.goTwo();
+            }
+        });
+
+
+        return view;
+    }
+
+}
